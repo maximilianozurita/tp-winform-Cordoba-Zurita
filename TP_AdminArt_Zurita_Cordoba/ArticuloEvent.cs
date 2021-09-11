42e241b03d2151279b0576fc1d5a8c5f4b8b50d3
Gabriel Cordoba <gabicordoba1998@gmail.com>
2021-09-10 23:17:22 -0300

    Prueba de lectrura de los Articulos

---
+++ TP_AdminArt_Zurita_Cordoba/ArticuloEvent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TP_AdminArt_Zurita_Cordoba
+{
+	class ArticuloEvent
+	{
+		public List<Articulo> listar()
+		{
+			List<Articulo> lista = new List<Articulo>();
+			SqlConnection conexion = new SqlConnection();
+			SqlCommand comando = new SqlCommand();
+			SqlDataReader lector;
+			try
+			{
+				conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_DB; integrated security=true";
+				comando.CommandType = System.Data.CommandType.Text;
+				comando.CommandText = "select Codigo, Nombre,Descripcion,ImagenUrl from ARTICULOS";
+				comando.Connection=conexion;
+				
+				conexion.Open();
+				lector = comando.ExecuteReader();
+
+				while (lector.Read())
+				{
+					Articulo aux = new Articulo();
+					aux.CodigoArticulo = (string)lector["Codigo"];
+					aux.Nombre = (string) lector ["Nombre"] ;
+					aux.Descripción= (string) lector["Descripcion"];
+					aux.Imagen = (string)lector["ImagenUrl"];
+
+					lista.Add(aux);
+
+                }
+
+
+
+				conexion.Close();
+					return lista;
+
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+		}
+    }
+
+}
--- TP_AdminArt_Zurita_Cordoba/Form1.Designer.cs
+++ TP_AdminArt_Zurita_Cordoba/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace TP_AdminArt_Zurita_Cordoba
 {
-    partial class Form1
+    partial class Form1Articulos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,52 @@ namespace TP_AdminArt_Zurita_Cordoba
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.pBoxArticulo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(0, 0);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.RowHeadersWidth = 51;
+            this.dgvArticulos.RowTemplate.Height = 24;
+            this.dgvArticulos.Size = new System.Drawing.Size(788, 150);
+            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
+            // 
+            // pBoxArticulo
+            // 
+            this.pBoxArticulo.Location = new System.Drawing.Point(0, 156);
+            this.pBoxArticulo.Name = "pBoxArticulo";
+            this.pBoxArticulo.Size = new System.Drawing.Size(607, 254);
+            this.pBoxArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxArticulo.TabIndex = 1;
+            this.pBoxArticulo.TabStop = false;
+            // 
+            // Form1Articulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pBoxArticulo);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "Form1Articulos";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1Articulos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxArticulo)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.PictureBox pBoxArticulo;
     }
 }
 
